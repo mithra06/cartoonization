c42414c402b94fb071070c1e9806c87e9e705988
mithra06 <80309986+mithra06@users.noreply.github.com>
2021-11-22 16:07:09 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,7 @@
-mkdir -p ~/.streamlit/
-echo "\
-[general]\n\
-email = \"colab.practice123@gmail.com\"\n\
-" > ~/.streamlit/credentials.toml
+mkdir -p ~/.streamlit
 
-echo "\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
+echo "[server]
+headless = true
+port = $PORT
+enableCORS = false
 " > ~/.streamlit/config.toml
