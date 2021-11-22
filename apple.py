ffddda38b572a2849aa790fc3bb8560d18fec654
mithra06 <80309986+mithra06@users.noreply.github.com>
2021-11-22 20:52:13 +0530

    Update apple.py

--- apple.py
+++ apple.py
@@ -2,7 +2,7 @@ import streamlit as st
 from multiapp import MultiApp
 from apps import cartoonify,filters,toonify
 
-app = MultiApp.MultiApp()
+app = MultiApp()
 app.add_app("filters", filters.app)
 app.add_app("cartoonify", cartoonify.app)
 app.add_app("toonify",toonify.app)
